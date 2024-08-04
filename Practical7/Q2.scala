f32c78422419e687ec2b0c112f7cc20310271b3d
Wameesha <140165428+Wameesha@users.noreply.github.com>
2024-08-04 14:00:02 +0530

    Add files via upload

---
+++ Practical7/Q2.scala
@@ -0,0 +1,11 @@
+object Q2{
+    def  calculateSquare(numbers: List[Int]): List[Int] ={
+        numbers.map(n => n * n)
+    }
+
+    def main(args: Array[String]): Unit = {
+        val input = List (1,2,3,4,5)
+        val output = calculateSquare(input)
+        println(output)
+    }
+}
